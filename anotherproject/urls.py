64e499088f6eea067dcfefc8e05b0a98511fdb60
mulyadiyaminsiahaan <112603524+mulyadiyaminsiahaan@users.noreply.github.com>
2025-03-15 20:48:17 +0700

    com

--- anotherproject/urls.py
+++ anotherproject/urls.py
@@ -19,7 +19,7 @@ from django.http import HttpResponse
 from django.urls import path
 
 def hello_world(request):
-    return HttpResponse("Hello All, Welcome to Django! This is my first project for deploy tO DOKKU")
+    return HttpResponse("Hello All, Welcome to Django! This is my first project for deploy tO DOKKU CAR OUTSIDE")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
